74042ff3bc1716f58d905261676373a3de64bc86
ShivaKrishnaReddyBurra <reddyburrashivakrishna@gmail.com>
2025-06-04 12:17:56 +0530

    update: Added title

--- src/pages/_document.js
+++ src/pages/_document.js
@@ -3,7 +3,9 @@ import { Html, Head, Main, NextScript } from 'next/document';
 export default function Document() {
   return (
     <Html lang="en">
-      <Head />
+      <Head 
+        title="SK Stories"
+      />
       <body className="bg-gradient-to-r from-orange-400 to-amber-300 text-gray-800">
         <Main />
         <NextScript />
